5753218977b7fe9ac4e3d6011154f5f9b893d380
Seonghyun Jung <tjdgusus@gmail.com>
2021-11-10 04:48:09 +0900

    현재 상영작 페이지 스크롤 구현

--- Test/realtime.js
+++ Test/realtime.js
@@ -38,3 +38,27 @@ $(function() {
    
 });
 
+
+(function() { // A   
+    const itemWrapperEl = document.querySelector('.row'),  
+          leftBtnEl = document.getElementById('left-btn'),  
+          rightBtnEl = document.getElementById('right-btn');  
+
+    function moveSlides(direction) { // B
+      const item = itemWrapperEl.querySelector('.card-view'),  
+            itemMargin = parseFloat(getComputedStyle(item).marginRight);
+            itemWidth = itemMargin + item.offsetWidth + 2; 
+
+      let itemCount = Math.round(itemWrapperEl.scrollLeft / itemWidth); 
+
+      if (direction === 'left') {
+        itemCount = itemCount - 1;
+      } else {
+        itemCount = itemCount + 1;
+      }
+      itemWrapperEl.scrollLeft = itemWidth * itemCount; 
+    }
+
+    leftBtnEl.addEventListener("click", e => moveSlides("left")); // C
+    rightBtnEl.addEventListener("click", e => moveSlides("right"));
+  });
